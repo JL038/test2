3b9fff00998126b3a18aca5dd72429f1a40c25ea
JL038 <jed.mhshmj.sk.s3k1s0m4j5@gmail.com>
2024-07-31 02:31:56 +0900

    追加してみた

---
+++ test/test.h
@@ -0,0 +1,18 @@
+//
+//  test.h
+//  test
+//
+//  Created by 江口柱利 on 2024/07/31.なんですのん
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for test.
+FOUNDATION_EXPORT double testVersionNumber;
+
+//! Project version string for test.
+FOUNDATION_EXPORT const unsigned char testVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <test/PublicHeader.h>
+
+
